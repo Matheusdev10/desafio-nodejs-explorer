e24664cb081ae15c44fd971e376b788f9a8e8a8b
Matheus <matheus_501@hotmail.com>
2022-10-03 23:00:29 -0300

    listando tags

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,11 @@
 const { Router } = require('express');
 const usersRouter = require('./users.routes.js');
 const notesRouter = require('./notes.routes');
+const tagsRouter = require('./tags.routes');
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/notes', notesRouter);
+routes.use('/tags', tagsRouter);
 
 module.exports = routes;
